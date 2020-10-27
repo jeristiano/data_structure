fc42b08d2f3ca0dcf5f0ac05ce8095bc0d9a15e6
jeristiano <jeremykuang@foxmail.com>
2020-10-27 14:17:38 +0800

    选择排序

--- SelectionSort/SelectionSort.php
+++ SelectionSort/SelectionSort.php
@@ -1,6 +1,10 @@
 <?php
 
 
+/**
+ * @param array $foo
+ * @return array
+ */
 function selectSort (array $foo)
 {
     //数组长度
@@ -23,9 +27,31 @@ function selectSort (array $foo)
         $foo[$i] = $foo[$minIndex];
         $foo[$minIndex] = $temp;
 
+        echo "第{$i}次循环后,数组内容为:";
+        printLoop($foo);
+
     }
     return $foo;
 }
 
 $arr = [3, 5, 7, 4, 9, 1, 2, 8];
-print_r(selectSort($arr));
+selectSort($arr);
+
+//打印结果
+//第0次循环后,数组内容为:1 5 7 4 9 3 2 8
+//第1次循环后,数组内容为:1 2 7 4 9 3 5 8
+//第2次循环后,数组内容为:1 2 3 4 9 7 5 8
+//第3次循环后,数组内容为:1 2 3 4 9 7 5 8
+//第4次循环后,数组内容为:1 2 3 4 5 7 9 8
+//第5次循环后,数组内容为:1 2 3 4 5 7 9 8
+//第6次循环后,数组内容为:1 2 3 4 5 7 8 9
+/**
+ * @param array $arr
+ */
+function printLoop (array $arr)
+{
+    foreach ($arr as $i) {
+        echo $i . ' ';
+    }
+    echo PHP_EOL;
+}
